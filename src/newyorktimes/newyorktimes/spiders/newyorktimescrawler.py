c15acf3a8e7942e18fddca69f35e5c1a055d0948
Nabila63Ahmed <nabila.ahmed63@gmail.com>
2017-11-08 11:37:30 +0200

    :tada: downloading and parsing articles into files

--- src/newyorktimes/newyorktimes/spiders/newyorktimescrawler.py
+++ src/newyorktimes/newyorktimes/spiders/newyorktimescrawler.py
@@ -10,15 +10,25 @@ class NewyorktimescrawlerSpider(CrawlSpider):
     idx = 0
     allowed_domains = ['www.nytimes.com']
     start_urls = ['https://www.nytimes.com/section/world/europe']
-    rules = (Rule(LinkExtractor(allow=[r'\d{4}/\d{2}/\d{2}/[a-z]+/[a-z]+/[^/]+']), callback="parse_item", follow=True),)
+    
+    rules = (Rule(LinkExtractor(allow=[r'\d{4}/\d{2}/\d{2}/[a-z]+/[a-z]+/[^/]+\.html']), callback="parse_item", follow=True),)
     def parse_item(self, response):
         self.log("Scraping: " + response.url)
 
         item = NewyorktimesItem()
         item['url'] = response.url
+        article = Article(item['url'], language='en')
+        article.download()
+        article.parse()
+        item['link_authors'] = ', '.join(article.authors)
+        item['link_title'] = article.title
+        text =  article.text
+                
+        file_name = '%d-%s'%(self.idx, item['link_title'])
+        file_text = 'Author(s): %s\nurl: %s\n\n%s'%(item['link_authors'], item['url'], text)
 
-        f = open('articles/%d'%(self.idx), 'w', encoding='utf8')
-        f.write(response.text)
+        f = open('articles/%s'%(file_name), 'w', encoding='utf8')
+        f.write(file_text)
         f.close()
         self.idx+=1
         return item
